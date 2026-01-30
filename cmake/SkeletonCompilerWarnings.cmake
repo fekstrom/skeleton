29865df80384df7fd1cc1b1c0c13724044548270
fekstrom <64606336+fekstrom@users.noreply.github.com>
2026-01-30 21:36:00 +0100

    Improve SkeletonCompilerWarnings.cmake

--- cmake/SkeletonCompilerWarnings.cmake
+++ cmake/SkeletonCompilerWarnings.cmake
@@ -1,11 +1,15 @@
+include(CMakeDependentOption)
+
 option(
     SKELETON_ENABLE_COMPILER_WARNINGS
     "Enable compiler warnings for the Skeleton build"
-    OFF
+    ${PROJECT_IS_TOP_LEVEL}
 )
-option(
+cmake_dependent_option(
     SKELETON_WARNINGS_AS_ERRORS
     "Treat compiler warnings as errors in the Skeleton build"
+    OFF
+    ${SKELETON_ENABLE_COMPILER_WARNINGS}
     OFF
 )
 
@@ -16,7 +20,13 @@ if(SKELETON_ENABLE_COMPILER_WARNINGS)
     if(CMAKE_CXX_COMPILER_ID MATCHES "^(AppleClang|Clang|GNU)$")
         target_compile_options(
             Skeleton_CompilerWarnings
-            INTERFACE -Wall -Wextra -Wpedantic -Wconversion
+            INTERFACE
+                -Wall
+                -Wextra
+                -Wpedantic
+                -Wconversion
+                -Wshadow
+                -Wold-style-cast
         )
     elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
         target_compile_options(
